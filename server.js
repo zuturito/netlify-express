d9eba13a99e378cac2d7ef0782f2269f5487ace7
Francisco Sánchez V <zuturito@hotmail.com>
2022-05-15 16:45:21 -0500

    cors
    cors requirements

--- server.js
+++ server.js
@@ -1,6 +1,20 @@
+var createError = require('http-errors');
 const express = require('express');
-const app = express();
+var path = require('path');
+var cookieParser = require('cookie-parser');
 const morgan=require('morgan');
+const cors = require('cors');
+const config = require('./config');
+const app = express();
+
+app.use( cors({ origin: true, credentials: true  }) );
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method');
+    res.header('Access-Control-Allow-Methods', 'GET');
+    res.header('Allow', 'GET');
+    next();
+});
 
 //Routes
 app.use(require('./routes/index'));
@@ -13,8 +27,11 @@ app.set('json spaces', 2);
 app.use(morgan('dev'));
 app.use(express.urlencoded({extended:false}));
 app.use(express.json());
- 
-//Iniciando el servidor
+
+ //Iniciando el servidor
 app.listen(app.get('port'),()=>{
     console.log(`Server listening on port ${app.get('port')}`);
 });
+
+
+
